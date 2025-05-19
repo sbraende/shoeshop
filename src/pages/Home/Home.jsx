f4fef72b29a0cc73e5e6ac4cd9ed3ceb00ee2a0e
sbraende <sebastian.braende@gmail.com>
2025-05-19 14:30:54 +0200

    feat: Setup and connect Firebase

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import { database } from "../../../firebase.config";
 import styles from "./Home.module.css";
+import { addDoc, collection } from "firebase/firestore";
 
 const Home = () => {
-  return <div>Home</div>;
+  console.log(database);
+
+  useEffect(() => {
+    addDoc(collection(database, "test"), {
+      platform: "PS5",
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
