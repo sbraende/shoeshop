63eeb9ad43f4ea215b02ac4d2fe44a55c4ba480e
sbraende <sebastian.braende@gmail.com>
2025-05-19 14:50:21 +0200

    feat: Connect to Google Gemini

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,26 @@ import { useEffect } from "react";
 import { database } from "../../../firebase.config";
 import styles from "./Home.module.css";
 import { addDoc, collection } from "firebase/firestore";
+import { ai } from "../../../gemini.config";
 
 const Home = () => {
-  console.log(database);
+  // console.log(database);
 
-  useEffect(() => {
-    addDoc(collection(database, "test"), {
-      platform: "PS5",
-    });
-  }, []);
+  // useEffect(() => {
+  //   addDoc(collection(database, "test"), {
+  //     platform: "PS5",
+  //   });
+  // }, []);
+
+  // const testAI = async () => {
+  //   const response = await ai.models.generateContent({
+  //     model: "gemini-2.0-flash",
+  //     contents: "Explain how AI works in a few words",
+  //   });
+  //   console.log(response);
+  //   console.log(response.text);
+  // };
+  // testAI();
 
   return (
     <div>
