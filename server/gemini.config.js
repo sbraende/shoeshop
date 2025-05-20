2fd107c0913bcb06763f59065a8823e3c58eb79b
sbraende <sebastian.braende@gmail.com>
2025-05-20 13:58:10 +0200

    feat: Implemnt server and client communication

--- server/gemini.config.js
+++ server/gemini.config.js
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
 
 export const ai = new GoogleGenAI({
-  apiKey: import.meta.env.VITE__GEMINI_API_KEY,
+  apiKey: process.env.GEMINI_API_KEY,
 });
--- server/server.js
+++ server/server.js
@@ -10,32 +10,6 @@ app.use(express.json());
 
 const PORT = 3001;
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-const geminiConfig = {
-  apiKey: process.env.GEMINI_API_KEY,
-};
-
-// app.get("/", async (req, res) => {
-//   try {
-//     // const response = await fetch();
-//     const data = {
-//       message: "Hi there from backend! Nodemon installed and working",
-//       apiKey: process.env.GEMINI_API_KEY,
-//     };
-//     res.json(data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-
 app.post("/", async (req, res) => {
   try {
     const data = {
